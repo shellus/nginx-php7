2d7fc9841f1544706dbba3717bcddca1e262ff0e
shellus <shellus@endaosi.com>
2016-06-22 17:02:19 +0800

    update

--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-#########################################################################
-# File Name: start.sh
-# Author: Skiychan
-# Email:  dev@skiy.net
-# Version:
-# Created Time: 2015/12/13
-#########################################################################
+
+set -e
 
 /usr/bin/supervisord -n -c /etc/supervisord.conf
